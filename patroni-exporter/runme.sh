c87f8f42f96a4b3a39a5924a1d37643eb16f93d4
Vladimir Bondar <vladimir@bondar.ru>
2023-10-17 18:11:10 +0300

    commit-2023-10-17-1811

--- patroni-exporter/runme.sh
+++ patroni-exporter/runme.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-sudo cp node_exporter.service /etc/systemd/system/node_exporter.service
-sudo systemctl start node_exporter
-sudo systemctl enable node_exporter
+sudo useradd -M -r -s /bin/false patroni_exporter
+sudo cp patroni_exporter /usr/local/bin/
+sudo cp patroni_exporter.service /etc/systemd/system/patroni_exporter.service
+sudo systemctl start patroni_exporter
+sudo systemctl enable patroni_exporter
