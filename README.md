66c941bb0f512e53bd23d855251d5428ded9a15a
Vladimir Bondar <vladimir@bondar.ru>
2023-10-24 10:35:43 +0300

    20231024-1035

--- README.md
+++ README.md
@@ -12,3 +12,14 @@ Installation:
     git clone https://github.com/vladimirbondar/dz2-prometheus.git
     cd dz2-prometheus/ansible
     ansible-playbook -i inventory prometheus-playbook.yml
+
+
+blackbox_exporter:
+
+Links:
+
+    https://github.com/prometheus/blackbox_exporter.git
+    https://github.com/prometheus/blackbox_exporter/releases/download/v0.24.0/blackbox_exporter-0.24.0.linux-amd64.tar.gz
+
+Installation:
+    
--- blackbox-exporter/runme.sh
+++ blackbox-exporter/runme.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-sudo useradd -M -r -s /bin/false node_exporter
-sudo cp node_exporter.service /etc/systemd/system/node_exporter.service
-sudo systemctl start node_exporter
-sudo systemctl enable node_exporter
+sudo useradd -M -r -s /bin/false blackbox_exporter
+sudo cp blackbox_exporter /usr/local/bin/
+sudo mkdir /opt/blackbox_exporter/
+sudo cp blackbox.yml /opt/blackbox_exporter/blackbox.yml
+sudo cp blackbox_exporter.service /etc/systemd/system/blackbox_exporter.service
+sudo systemctl start blackbox_exporter
+sudo systemctl enable blackbox_exporter
